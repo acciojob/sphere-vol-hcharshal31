62d0a98fd93c8aa19375a44b214e66959c59f4a8
Harshal Chavan <91681985+hcharshal31@users.noreply.github.com>
2025-01-06 01:05:50 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,15 +1,13 @@
-function volume_sphere() {
+function volume_sphere(event) {
+    event.preventDefault();  // Add this line to prevent form submission
     const radiusValue = parseFloat(document.getElementById("radius").value);
-	const volume = document.getElementById("volume");
-	if(radiusValue < 0 || isNaN(radiusValue)){
-		 volume.value = NaN;
-		return;
-	}
-	const sphereVolume = (4 / 3) * Math.PI * Math.pow(radiusValue, 3);
-	volume.value = sphereVolume.toFixed(4);
-	
+    const volume = document.getElementById("volume");
+    if(radiusValue < 0 || isNaN(radiusValue)){
+        volume.value = "NaN";
+        return;
+    }
+    const sphereVolume = (4 / 3) * Math.PI * Math.pow(radiusValue, 3);
+    volume.value = sphereVolume.toFixed(4);
 } 
 
-document.getElementById("submit").onclick = volume_sphere;
-
-window.onload = document.getElementById('MyForm').onsubmit = volume_sphere;
+document.getElementById("MyForm").onsubmit = volume_sphere;
