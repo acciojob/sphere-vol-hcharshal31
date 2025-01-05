3325492608029e97fae1f5559f0aeef12b7d6017
Harshal Chavan <91681985+hcharshal31@users.noreply.github.com>
2025-01-06 00:42:34 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,6 +1,13 @@
 function volume_sphere() {
-    //Write your code here
-  
+    const radiusValue = parseFloat(document.getElementById("radius").value);
+	const volume = document.getElementById("volume");
+	if(radiusValue < 0 || isNaN(radiusValue)){
+		 volume.value = NaN;
+		return;
+	}
+	const sphereVolume = (4 / 3) * Math.PI * Math.pow(radiusValue, 3);
+	volume.value = sphereVolume.toFixed(4);
+	
 } 
 
-window.onload = document.getElementById('MyForm').onsubmit = volume_sphere;
+window.onload = document.getElementById('MyForm').onsubmit = volume_sphere();
